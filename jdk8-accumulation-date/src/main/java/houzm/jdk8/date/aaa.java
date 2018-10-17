66931457e03db0529f745753b41b45548d03dd5a
hzm-January <hzm_dream@163.com>
2018-10-17 13:46:07 +0800

    commit

---
+++ jdk8-accumulation-date/src/main/java/houzm/jdk8/date/aaa.java
@@ -0,0 +1,13 @@
+package houzm.jdk8.date;
+
+/**
+ * Package: houzm.jdk8.date
+ * Author: houzm
+ * Date: Created in 2018/10/15 17:01
+ * Copyright: Copyright (c) 2018
+ * Version: 0.0.1
+ * Modified By:
+ * Description： TODO
+ */
+public class aaa {
+}
---
+++ jdk8-accumulation-date/src/main/java/houzm/jdk8/date/joda/DateTimeApi.java
@@ -0,0 +1,41 @@
+package houzm.jdk8.date.joda;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeField;
+import org.joda.time.DateTimeFieldType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Package: houzm.jdk8.date.joda
+ * Author: houzm
+ * Date: Created in 2018/10/15 17:01
+ * Copyright: Copyright (c) 2018
+ * Version: 0.0.1
+ * Modified By:
+ * Description： test joda date api---->datetime
+ */
+public class DateTimeApi {
+    private static Logger logger = LoggerFactory.getLogger(DateTimeApi.class);
+    public static void main(String[] args) {
+        DateTime dateTime = new DateTime();
+        logger.debug(dateTime.toString("yyyy-MM-dd HH:mm:ss"));
+        logger.debug("{}", dateTime.getEra());
+        logger.debug("{}", dateTime.getCenturyOfEra()); //20
+        logger.debug("{}", dateTime.getChronology()); //ISOChronology[Asia/Shanghai]
+        logger.debug("{}", dateTime.getMillis()); //获取从 Java 纪元开始到现在 datetime 即时时间的毫秒* 1970-01-01T00:00:00Z
+        logger.debug("{}", dateTime.getZone()); //Asia/Shanghai
+        logger.debug("{}", dateTime.getMillisOfDay()); //62153600
+
+        logger.debug("{}", dateTime.getDayOfYear()); //288
+        logger.debug("{}", dateTime.getHourOfDay()); //0
+        logger.debug("{}", dateTime.getMillisOfSecond()); //0
+        logger.debug("{}", dateTime.getYear()); //288
+        logger.debug("{}", dateTime.getMonthOfYear()); //288
+        logger.debug("{}", dateTime.getDayOfMonth()); //288
+
+        logger.debug("当天凌晨时间：{}", new DateTime(dateTime.getMillis()-dateTime.getMillisOfDay())); //288
+        logger.debug("明天凌晨时间：{}", new DateTime(dateTime.getMillis()-dateTime.getMillisOfDay()).plusDays(1)); //288
+
+    }
+}
